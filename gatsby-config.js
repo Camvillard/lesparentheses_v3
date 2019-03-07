b7432dc4ed31855da650eb87330f846303d1f5b0
camille villard <hello@camillevillard.com>
2019-03-06 19:28:52 -0500

    wordpress install

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,67 +1,88 @@
+const config = require('./data/SiteConfig')
+
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
+
 module.exports = {
-  plugins: [`gatsby-plugin-sass`,
+  pathPrefix: config.pathPrefix,
+  siteMetadata: {
+    siteUrl: config.siteUrl + pathPrefix,
+    rssMetadata: {
+      site_url: config.siteUrl + pathPrefix,
+      feed_url: config.siteUrl + pathPrefix + config.siteRss,
+      title: config.siteTitle,
+      description: config.siteDescription,
+      image_url: `${config.siteUrl + pathPrefix}/logos/logo-512.png`,
+      author: config.userName,
+      copyright: config.copyright
+    }
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
     {
-       resolve: `gatsby-source-filesystem`,
-       options: {
-         name: `src`,
-         path: `${__dirname}/src/`,
-       },
-     },
-
-    {
-      resolve: "gatsby-source-wordpress",
+      resolve: 'gatsby-source-wordpress',
       options: {
-        /*
-         * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
-         * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
-         */
-        baseUrl: "les-parentheses.com",
-        // The protocol. This can be http or https.
-        protocol: "https",
-        // Indicates whether the site is hosted on wordpress.com.
-        // If false, then the assumption is made that the site is self hosted.
-        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
-        // If your site is hosted on wordpress.org, then set this to false.
+        // The base url to your WP site.
+        baseUrl: 'parentheses.camillevillard.com',
+        // baseUrl: 'wpgatsby.wtf',
+        // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
-        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
-        // This feature is untested for sites hosted on wordpress.com.
-        // Defaults to true.
+        // The protocol. This can be http or https.
+        protocol: 'http',
+        // Use 'Advanced Custom Fields' Wordpress plugin
         useACF: false,
-        // Include specific ACF Option Pages that have a set post ID
-        // Regardless if an ID is set, the default options route will still be retrieved
-        // Must be using V3 of ACF to REST to include these routes
-        // Example: `["option_page_1", "option_page_2"]` will include the proper ACF option
-        // routes with the ID option_page_1 and option_page_2
-        // Dashes in IDs will be converted to underscores for use in GraphQL
-        acfOptionPageIds: [],
-
-        // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
-        // It can help you debug specific API Endpoints problems.
+        auth: {},
+        // Set to true to debug endpoints on 'gatsby build'
         verboseOutput: true,
-        // Set how many pages are retrieved per API request.
-        perPage: 100,
-        // Set how many simultaneous requests are sent at once.
-        concurrentRequests: 10,
-        // Set WP REST API routes whitelists
-        // and blacklists using glob patterns.
-        // Defaults to whitelist the routes shown
-        // in the example below.
-        // See: https://github.com/isaacs/minimatch
-        // Example:  `["/*/*/comments", "/yoast/**"]`
-        // ` will either include or exclude routes ending in `comments` and
-        // all routes that begin with `yoast` from fetch.
-        // Whitelisted routes using glob patterns
-        includedRoutes: [
-          "**/*/*/pages",
+        excludedRoutes: [
+          "/*/*/comments",
+          "/yoast/**",
+          "/*/*/users",
+          "/*/users/me",
+          "/oembed/*"
         ],
-        // Blacklisted routes using glob patterns
-        excludedRoutes: ["**/*/*/posts/1456"],
-        // use a custom normalizer which is applied after the built-in ones.
-        normalizer: function({ entities }) {
-          return entities
-        },
-      },
+      }
     },
-  ],
-
+    // {
+    //   resolve: 'gatsby-plugin-google-analytics',
+    //   options: {
+    //     trackingId: config.googleAnalyticsID,
+    //   },
+    // },
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        color: config.themeColor
+      }
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-twitter',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: config.siteTitle,
+        short_name: config.siteTitle,
+        description: config.siteDescription,
+        start_url: config.pathPrefix,
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: 'minimal-ui',
+        icons: [
+          {
+            src: '/logos/logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logos/logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    },
+    'gatsby-plugin-offline'
+  ]
 }
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import '../styles/global.scss';
-
-import Header from "../templates/header"
-
-export default () => (
-  <Header />
-  )
